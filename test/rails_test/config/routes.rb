c2b80eb1f1e112ef2c8990b5cc5bb5b405092cc9
Justin Ball <justinball@gmail.com>
2010-10-13 17:59:50 -0600

    Transition to Rails 3 and RSpec

--- test/rails_test/config/routes.rb
+++ test/rails_test/config/routes.rb
@@ -1,8 +1,10 @@
 RailsTest::Application.routes.draw do
+  
   root :to => "default#index"
+  
   resources :users do
     resources :posts, :controller => 'muck/posts'
-    resource :blog, :controller => 'muck/blogs'
+    resource :blog, :controller => 'muck/blogs'do
       resources :posts, :controller => 'muck/posts'
     end
   end
